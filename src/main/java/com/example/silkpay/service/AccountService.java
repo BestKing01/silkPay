398727b4a07b2e47d3973fb5725883e5ba7c3245
Temirbayev Temirlan <temirbayev002@gmail.com>
2023-07-16 16:16:54 +0600

    Добавление Аннотации в model для сокращения кода. Убрал несколько лишних строк

--- src/main/java/com/example/silkpay/service/AccountService.java
+++ src/main/java/com/example/silkpay/service/AccountService.java
@@ -1,7 +1,6 @@
 package com.example.silkpay.service;
 
 import com.example.silkpay.model.Account;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -9,10 +8,10 @@ import java.util.Optional;
 
 
 public interface AccountService {
-    public List<Account> allAccount();
-    public Account createAccount(Account account);
-    public Optional<Account> getAccountById(Long accountId);
+    List<Account> allAccount();
+    Account createAccount(Account account);
+    Optional<Account> getAccountById(Long accountId);
 
-    public BigDecimal getAccountBalanceByNumber(String accountNumber);
-    public void transferFunds(Long sourceAccountId, Long targetAccountId, BigDecimal amount);
+    BigDecimal getAccountBalanceByNumber(String accountNumber);
+    void transferFunds(Long sourceAccountId, Long targetAccountId, BigDecimal amount);
 }
